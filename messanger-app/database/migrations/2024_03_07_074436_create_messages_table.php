d11d99c712eb4a1a72c6d74eae89730559af1e51
SalmaRedaBedair <salmaredab173@gmail.com>
2024-03-07 09:46:58 +0200

    upload

---
+++ messanger-app/database/migrations/2024_03_07_074436_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('conversation_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+            $table->text('body');
+            $table->emum('type',['text','attachment'])
+                ->default('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
---
+++ notes.md
@@ -0,0 +1,7 @@
+# make models and migrations
+```
+php artisan make:model Conversation -m
+php artisan make:migration create_participants_table
+php artisan make:model Message -m
+php artisan make:migration create_recipients_table
+```
